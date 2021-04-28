8ef124b121e5a1e4bdd1ab7c7f00c067333ded76
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-04-28 08:54:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ ctx = canvas.getContext("2d");
 
 car1_width = 120;
 car1_height = 70;
-car1_image = "https://i.postimg.cc/9rqYz9HM/car1.png";
+car1_image = "https://postimg.cc/9rqYz9HM";
 car1_x = 10;
 car1_y = 10;
 
 car2_width = 120;
 car2_height = 70;
-car2_image = "https://i.postimg.cc/tnnW1Kff/car2.png";
+car2_image = "https://i.postimg.cc/tnnW1Kff";
 car2_x = 10;
 car2_y = 100;
 
